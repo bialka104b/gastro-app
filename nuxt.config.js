80be7e10aeec688f170c72ee48dcb1adecd75ed4
Marta J-Kulig <bialka104b@interia.pl>
2025-09-04 21:11:00 +0200

    [GASTRO] doinstalowanie pwa

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
 	compatibilityDate: "2025-07-15",
 	devtools: { enabled: true },
 	ssr: true,
-	modules: ["@nuxt/image", "@nuxt/fonts", "@nuxt/scripts", "@nuxtjs/tailwindcss"],
+	modules: ["@vite-pwa/nuxt", "@nuxt/image", "@nuxt/fonts", "@nuxt/scripts", "@nuxtjs/tailwindcss"],
 	css: ["./scss/main.scss"],
 	tailwindcss: {
 		configPath: "./tailwind.config.js",
@@ -17,12 +17,39 @@ export default defineNuxtConfig({
 			},
 		},
 	},
-	// pwa: {
-	// 	workbox: {
-	// 		assetsURLPattern: "/",
-	// 		publicPath: "/",
-	// 	},
-	// },
+	pwa: {
+		registerType: "autoUpdate", // Service worker będzie aktualizował się automatycznie
+		manifest: {
+			name: "Gastro App",
+			short_name: "gastro_app",
+			description: "Moja aplikacja PWA w Nuxt 4",
+			theme_color: "#1e40af",
+			background_color: "#ffffff",
+			icons: [
+				{ src: "/pwa-192x192.png", sizes: "192x192", type: "image/png" },
+				{ src: "/pwa-512x512.png", sizes: "512x512", type: "image/png" },
+			],
+		},
+		workbox: {
+			// Definiuj strategie cache (opcjonalnie)
+			runtimeCaching: [
+				{
+					urlPattern: /^https:\/\/.*\.(png|jpg|css|js)$/,
+					handler: "CacheFirst",
+					options: {
+						cacheName: "static-assets",
+						expiration: { maxEntries: 60, maxAgeSeconds: 3600 },
+					},
+				},
+			],
+		},
+		client: {
+			installPrompt: true,
+		},
+		devOptions: {
+			enabled: true,
+		},
+	},
 	alias: {
 		style: fileURLToPath(new URL("./scss", import.meta.url)),
 	},
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="p-6">
 
-    <h1 class="text-2xl font-bold text-primary">Witaj w Gastro App 🍕</h1>
-    <p class="mt-2 text-gray-600">
-      To jest strona startowa renderowana automatycznie z folderu <code>pages/</code>.
+    <h1 class="text-2xl font-bold text-red-500">
+      Witaj w Gastro App 🍕
+    </h1>
+    <p class="mt-2 text-gray-600 md:bg-gray-700">
+      To jest strona startowa z folderu <code>pages/</code>.
     </p>
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
